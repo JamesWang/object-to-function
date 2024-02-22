a9c352d6e95bda901cc2b0fd8d1ecfe50eebb7c2
DESKTOP-84BFPQT\james <james.wang.w@gmail.com>
2024-02-21 19:45:37 -0500

    keep going

--- src/main/scala/com/aidokay/o2f/domain/acct/AccountService.scala
+++ src/main/scala/com/aidokay/o2f/domain/acct/AccountService.scala
@@ -1,8 +1,9 @@
 package com.aidokay.o2f.domain.acct
 
-import com.aidokay.o2f.domain.acct.models.{Account, Amount, Balance, today}
+import com.aidokay.o2f.domain.acct.models.{Account, Amount, Balance, CheckingAccount, today}
+
 import java.util.Date
-import scala.util.{Success, Failure, Try}
+import scala.util.{Failure, Success, Try}
 
 trait AccountService[Account, Amount, Balance] {
   def open(no: String, name: String, openData: Option[Date]): Try[Account]
@@ -26,26 +27,26 @@ object AccountService extends AccountService[Account, Amount, Balance] {
     if (no.isEmpty || name.isEmpty)
       Failure(new Exception(s"Account no or name cannot be blank"))
     else if (openDate.getOrElse(today) before today)
-      Failure(new Exception(s"Cannot oepn account in the past"))
-    else Success(Account(no, name, openDate.getOrElse(today)))
+      Failure(new Exception(s"Cannot open account in the past"))
+    else Account.checkingAccount(no, name, openDate)
   }
 
   override def close(account: Account, closeDate: Option[Date]): Try[Account] = {
     val cd = closeDate.getOrElse(today)
-    if (cd before account.dataOfOpen)
-      Failure(new Exception(s"Close date $cd cannot be before opening date ${account.dataOfOpen}"))
-    else Success(account.copy(dateOfClose = Some(cd)))
+    if (cd before account.dateOfOpen.getOrElse(today))
+      Failure(new Exception(s"Close date $cd cannot be before opening date ${account.dateOfOpen}"))
+    else Success(account.asInstanceOf[CheckingAccount].copy(dateOfClose = Some(cd)))
   }
 
   override def debit(account: Account, amount: Amount): Try[Account] = {
     if (account.balance.amount < amount)
       Failure(new Exception("Insufficient balance"))
     else
-      Success(account.copy(balance = Balance(account.balance.amount - amount)))
+      Success(account.asInstanceOf[CheckingAccount].copy(balance = Balance(account.balance.amount - amount)))
   }
 
   override def credit(account: Account, amount: Amount): Try[Account] =
-    Success(account.copy(balance = Balance(account.balance.amount + amount)))
+    Success(account.asInstanceOf[CheckingAccount].copy(balance = Balance(account.balance.amount + amount)))
 
   override def balance(account: Account): Try[Balance] = Success(account.balance)
 }
--- src/main/scala/com/aidokay/o2f/domain/acct/models.scala
+++ src/main/scala/com/aidokay/o2f/domain/acct/models.scala
@@ -1,6 +1,7 @@
 package com.aidokay.o2f.domain.acct
 
 import java.util.{Calendar, Date}
+import scala.util.{Failure, Success, Try}
 
 object models {
   type Amount = BigDecimal
@@ -9,6 +10,68 @@ object models {
 
   case class Balance(amount: Amount = 0)
 
-  case class Account(no: String, name: String, dataOfOpen: Date, dateOfClose: Option[Date] = None, balance: Balance = Balance(0))
+  sealed trait Account {
+    def no: String
 
+    def name: String
+
+    def dateOfOpen: Option[Date]
+
+    def dateOfClose: Option[Date]
+
+    def balance: Balance
+  }
+
+  final case class CheckingAccount (no: String, name: String, dateOfOpen: Option[Date], dateOfClose: Option[Date], balance: Balance) extends Account
+
+  private final case class SavingsAccount (no: String, name: String, rateOfInterest: Amount, dateOfOpen: Option[Date], dateOfClose: Option[Date], balance: Balance) extends Account
+
+  object Account {
+    def checkingAccount(no: String, name: String, openDate: Option[Date], closeDate: Option[Date] = None, balance: Balance = Balance(0)): Try[Account] = {
+      closeDateCheck(openDate, closeDate) map { d => CheckingAccount(no, name, Some(d._1), d._2, balance) }
+    }
+
+    def savingsAccount(no: String, name: String, rate: BigDecimal,
+                       openDate: Option[Date], closeDate: Option[Date] = None,
+                       balance: Balance = Balance(0)): Try[Account] = {
+      closeDateCheck(openDate, closeDate).map { d =>
+        if (rate <= BigDecimal(0))
+          throw new Exception(s"Interest rate $rate must be > 0")
+        else
+          SavingsAccount(no, name, rate, Some(d._1), d._2, balance)
+      }
+    }
+    private def closeDateCheck(openDate: Option[Date], closeDate: Option[Date]): Try[(Date, Option[Date])] = {
+      val od = openDate.getOrElse(today)
+      closeDate.map { cd =>
+        if (cd before od)
+          Failure(new Exception(s"Close date[$cd] cannot be earlier than open date [$od]"))
+        else Success((od, Some(cd)))
+      }.getOrElse(Success(od, closeDate))
+    }
+  }
+
+  sealed trait DayOfWeek {
+    val value: Int
+
+    override def toString: String = value match {
+      case 1 => "Monday"
+      case 2 => "Tuesday"
+      case 3 => "Wednesday"
+      case 4 => "Thursday"
+      case 5 => "Friday"
+      case 6 => "Saturday"
+      case 7 => "Sunday"
+    }
+  }
+
+  object DayOfWeek {
+    private def unsafeDayOfWeek(day: Int) = new DayOfWeek {
+      val value: Int = day
+    }
+
+    private val isValid: Int => Boolean = { i => i >= 1 && i <= 7 }
+
+    def dayOfWeek(day: Int): Option[DayOfWeek] = if (isValid(day)) Some(unsafeDayOfWeek(day)) else None
+  }
 }
